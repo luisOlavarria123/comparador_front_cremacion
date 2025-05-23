8ab0b11ed8d8729c23abe50a6a9bd3f47dcbab26
Luis Olavarria <luis.olavarria@live.com>
2025-05-22 20:51:33 -0400

    feat: add terminos y condiciones

--- comparadorApp/urls.py
+++ comparadorApp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('compara/', views.comparador, name='compara'),
     path('blog', views.blog, name='blog'),
+    path('terminosCondiciones', views.terminosCondiciones, name='terminosCondiciones'),
     path('noticias/<slug:slug>/', views.detalle_noticia_template, name='detalle-noticia-template'),
     path('noticias/<slug:slug>/', views.detalle_noticia_template, name='noticia_detalle'),
 ]
--- comparadorApp/views.py
+++ comparadorApp/views.py
@@ -9,8 +9,11 @@ def index(request):
     return render(request,'index.html')
 
 def blog(request):
-
     return render(request,'blog/noticia_list.html')
 
 def detalle_noticia_template(request, slug):
     return render(request, 'blog/noticia_detail.html', {'slug': slug})
+
+def terminosCondiciones(request):
+    return render(request,'terminosCondiciones.html')
+
